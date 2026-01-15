b85f4144590a86b22b5ade0b32c46ccfb3e9e41f
Shahid <109452231+raza-shahi@users.noreply.github.com>
2026-01-15 06:15:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+const players = {};
+const usersElement = document.getElementById("users");
+const gameBoardElement = document.getElementById("game-board");
+const user1Input = document.getElementById('player-1');
+const user2Input = document.getElementById('player-2');
+let currentPlayer = document.getElementById('current-player');
+
+document.getElementById('submit').addEventListener('click', function() {
+    const user1 = user1Input.value;
+    const user2 = user2Input.value;
+    if(user1 && user2) {
+        players.player1 = user1;
+        players.player2 = user2;
+        alert(`Players set:\nPlayer 1: ${players.player1}\nPlayer 2: ${players.player2}`);
+        usersElement.style.display = "none";
+        currentPlayer.textContent = `${players.player1}, you're up!`;
+        gameBoardElement.style.display = "block";
+    } else {
+        alert('Please enter names for both players.');
+    }
+
+});
